33f048ca1253b8cc45b326c788c612a83e8f8bbe
pedrozampi <p.jzampieri@gmail.com>
2026-02-02 16:37:48 -0300

    Fix: register as body

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from strategy import hash_password, verify_password, create_access_token
 from jose import JWTError, jwt
 from config import settings
 from pymongo import MongoClient
+from pydantic import BaseModel, Field
+
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -13,32 +15,34 @@ SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 client = MongoClient(settings.MONGO_URI)
 db = client[settings.MONGO_DB]
 users_collection = db["users"]
 
+class User(BaseModel):
+    username: str = Field(default=None)
+    password: str = Field(default=None)
 
-@router.post("/register")
-async def register(username: str, password: str):
-    hashed_password = hash_password(password)
-    user = users_collection.find_one({"username": username})
-    if user:
+@router.post("/register", tags=["auth"], description="Register a new user", summary="Register a new user")
+async def register(user: User):
+    hashed_password = hash_password(user.password)
+    user_db = users_collection.find_one({"username": user.username})
+    if user_db:
         raise HTTPException(status_code=400, detail="User already exists")
-    result = users_collection.insert_one({"username": username, "hashed_password": hashed_password})
+    result = users_collection.insert_one({"username": user.username, "hashed_password": hashed_password})
 
     return {"message": "User registered successfully"}
 
-@router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+@router.post("/token", tags=["auth"], description="Login a user", summary="Login a user")
+async def login(form_data: User = Depends()):
     user = users_collection.find_one({"username": form_data.username})
     if not user or not verify_password(form_data.password, user["hashed_password"]):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     access_token = create_access_token(data={"sub": user["username"], "id": str(user["_id"])})
     return {"access_token": access_token, "token_type": "bearer"}
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -48,7 +52,3 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     except JWTError:
         raise HTTPException(status_code=401, detail="Could not validate credentials")
     return {"username": username, "id": user_id}
-
-@router.get("/users/me")
-async def read_users_me(current_user: dict = Depends(get_current_user)):
-    return current_user
